716d9ec30f3b079d644bff2a13310e81f3711bbe
Jason Bittel <jason.bittel@gmail.com>
2010-03-24 05:34:41 +0000

    Implemented rudimentary card dealing

--- main.lua
+++ main.lua
@@ -1,22 +1,45 @@
 
 cards = {}
+players = {}
 
 FACES = { '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A' }
 SUITS = { 'C', 'D', 'H', 'S' }
-NUMDECKS = 2
+
+NUM_DECKS = 3
+NUM_PLAYERS = 5
+
+function init_game()
+    build_decks(NUM_DECKS)
+
+    math.randomseed(os.time())
+    shuffle(cards)
+
+    deal_cards(NUM_PLAYERS)
+end
 
 function build_decks(num)
     for deck = 1,num do
         for _,suit in ipairs(SUITS) do
             for rank,face in ipairs(FACES) do
                 local card = {}
-                table.insert(cards,card)
+                table.insert(cards, card)
                 card.suit = suit
                 card.face = face
                 card.rank  = rank + 1
                 card.deck = deck
             end
         end
+    end
+end
+
+function deal_cards(num)
+    for i = 1,num do
+        local player = {}
+        table.insert(players, player)
+        player.num = i
+        player.hidden = get_cards(cards, 3)
+        player.visible = get_cards(cards, 3)
+        player.hand = get_cards(cards, 3)
     end
 end
 
@@ -30,13 +53,26 @@ function shuffle(cards)
     end
 end
 
-build_decks(NUMDECKS)
+function get_cards(cards, num)
+    local t = {}
+    for i = 1,num do
+        local card = table.remove(cards)
+        table.insert(t, card)
+    end
 
-math.randomseed(os.time())
-shuffle(cards)
-
-for _,card in ipairs(cards) do
-    print(card.face..card.suit..' ['..card.rank..']')
+    return t
 end
 
-print('Total cards: '..#cards)
+function get_next_card(cards)
+    return table.remove(cards)
+end
+
+init_game()
+
+for _,player in ipairs(players) do
+    io.write('['..player.num..'] ')
+    for _,card in ipairs(player.hand) do
+        io.write(card.face..card.suit..' ')
+    end
+    print('')
+end
