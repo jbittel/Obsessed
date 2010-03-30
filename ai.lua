869f7967f9c52d4689ece050d253c1b1b1649db5
Jason Bittel <jason.bittel@gmail.com>
2010-03-30 00:02:05 +0000

    AI now plays all available cards of the selected face

--- ai.lua
+++ ai.lua
@@ -1,3 +1,6 @@
+-- TODO play cards according to an assigned value
+-- TODO prioritize 4+ stacks of non-special cards
+-- TODO only(?) play stacks of non-special cards
 function play_ai(pile, hand)
     local num = #hand
     for i=1,num do
@@ -5,6 +8,12 @@ function play_ai(pile, hand)
         hand[i].play = true
     
         if is_valid_play(pile, hand) then
+            -- Play all cards of the selected face
+            for j=1,num do
+                if hand[j].face == hand[i].face then
+                    hand[j].play = true
+                end
+            end
             return
         end
     end
